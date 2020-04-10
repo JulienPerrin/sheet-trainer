d3937f33a5c4c3727ec6d2ee6ad15b4754bb615c
JulienPerrin <julien.perrin25@gmail.com>
2020-04-10 21:33:17 +0200

    added e2e tests + analyser

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,5 +1,13 @@
 import { AppPage } from "./app.po";
 import { browser, logging } from "protractor";
+import * as fs from "fs";
+
+// abstract writing screen shot to a file
+function writeScreenShot(extension: string, filename: string) {
+  const stream = fs.createWriteStream("e2e/data/" + filename);
+  stream.write(Buffer.from(extension, "base64"));
+  stream.end();
+}
 
 describe("workspace-project App", () => {
   let page: AppPage;
@@ -8,20 +16,64 @@ describe("workspace-project App", () => {
     page = new AppPage();
   });
 
-  it("should display welcome message", () => {
+  it("should count right and wrong properly", () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual("sheet-trainer app is running!");
+    browser.takeScreenshot().then((png) => {
+      writeScreenShot(png, "home.png");
+    });
+    expect(page.getRight()).toBe(0);
+    expect(page.getWrong()).toBe(0);
+
+    page.getPiano().click();
+    expect(page.getRightPlusWrong()).toBe(1);
+
+    page.getPiano().click();
+    expect(page.getRightPlusWrong()).toBe(2);
+
+    page.getPiano().click();
+    expect(page.getRightPlusWrong()).toBe(3);
+
+    page.getPiano().click();
+    expect(page.getRightPlusWrong()).toBe(4);
+
+    page.getPiano().click();
+    expect(page.getRightPlusWrong()).toBe(5);
+
+    page.getPiano().click();
+    expect(page.getRightPlusWrong()).toBe(6);
+
+    page.getPiano().click();
+    expect(page.getRightPlusWrong()).toBe(7);
+
+    page.getPiano().click();
+    expect(page.getRightPlusWrong()).toBe(8);
+
+    browser.takeScreenshot().then((png) => {
+      writeScreenShot(png, "home2.png");
+    });
+  });
+
+  it("should change first note after 8 tries", () => {
+    page.navigateTo();
+    browser.takeScreenshot().then((png) => {
+      writeScreenShot(png, "home.png");
+    });
+    expect(page.getFirstNote()).toBe("vf-auto1006");
+    for (let i = 0; i < 8; i++) {
+      page.getPiano().click();
+    }
+    expect(page.getFirstNote()).toBe("vf-auto1100");
+    browser.takeScreenshot().then((png) => {
+      writeScreenShot(png, "home2.png");
+    });
   });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
-    const logs = await browser
-      .manage()
-      .logs()
-      .get(logging.Type.BROWSER);
+    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
     expect(logs).not.toContain(
       jasmine.objectContaining({
-        level: logging.Level.SEVERE
+        level: logging.Level.SEVERE,
       } as logging.Entry)
     );
   });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,12 +1,32 @@
-import { browser, by, element } from "protractor";
+import { browser, by, element, ElementFinder } from "protractor";
 
 export class AppPage {
   navigateTo(): Promise<unknown> {
     return browser.get(browser.baseUrl) as Promise<unknown>;
   }
 
-  getTitleText(): Promise<string> {
-    return element(by.css("app-root .content span")).getText() as Promise<
+  getPiano(): ElementFinder {
+    return element(by.className("C4"));
+  }
+
+  getRight(): Promise<number> {
+    return element(by.css("#right"))
+      .getText()
+      .then((x) => +x) as Promise<number>;
+  }
+
+  getWrong(): Promise<number> {
+    return element(by.css("#wrong"))
+      .getText()
+      .then((x) => +x) as Promise<number>;
+  }
+
+  getRightPlusWrong(): Promise<number> {
+    return this.getRight().then((x) => this.getWrong().then((y) => x + y));
+  }
+
+  getFirstNote(): Promise<string> {
+    return element(by.className("vf-stavenote")).getAttribute("id") as Promise<
       string
     >;
   }
