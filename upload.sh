3524926e1259f536ed4397d9315b61055c4a1bfe
JulienPerrin <julien.perrin25@gmail.com>
2021-02-07 21:19:27 +0100

    correct Jenkins sudo: not found

--- upload.sh
+++ upload.sh
@@ -4,4 +4,4 @@
 cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" || exit
 
 ng build --prod --aot=true --buildOptimizer=true
-sudo rsync -e "ssh" -r pi@8raspberry:/home/pi/programmation/sheet-trainer/dist ./dist
+rsync -e "ssh" -r pi@8raspberry:/home/pi/programmation/sheet-trainer/dist ./dist
