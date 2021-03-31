a1e04eb9d0c9ffcdbea9281490e49a7ce8a357fd
JulienPerrin <julien.perrin25@gmail.com>
2021-03-31 23:35:42 +0200

    chgt location serveur

--- upload.sh
+++ upload.sh
@@ -7,5 +7,5 @@ cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" || exit
 # get project dependancies
 npm install
 
-ng build --prod --aot=true --buildOptimizer=true
-rsync -e "ssh" -r ./dist pi@8raspberry:/home/pi/programmation/sheet-trainer/
+npm run build:prod
+rsync -e "ssh" -r ./dist pi@8raspberry:/home/pi/programmation/owncloud_with_docker
